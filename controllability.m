d8777056afa8fde41a03b7bd99c5162edda3fb9e
dhartnet <161893432+dhartnet@users.noreply.github.com>
2024-12-18 10:31:10 -0500

    Update controllability.m

--- controllability.m
+++ controllability.m
@@ -6,6 +6,7 @@ syms l1;
 syms l2;
 syms g;
 
+% State Space representation matrices
 A = [0 1 0 0 0 0; 
     0 0 -m1*g/M 0 -m2*g/M 0; 
     0 0 0 1 0 0; 
@@ -20,12 +21,14 @@ B = [0;
     0; 
     1/(M*l2)];
 
+% defining controllability matrix columns
 e2 = A*B;
 e3 = A^(2)*B;
 e4 = A^(3)*B;
 e5 = A^(4)*B;
 e6 = A^(5)*B;
 
+% Controllability matrix
 E = [B(1,1) e2(1,1) e3(1, 1) e4(1, 1) e5(1, 1) e6(1, 1); 
     B(2,1) e2(2,1) e3(2, 1) e4(2, 1) e5(2, 1) e6(2, 1); 
     B(3,1) e2(3,1) e3(3, 1) e4(3, 1) e5(3, 1) e6(3, 1); 
@@ -33,4 +36,5 @@ E = [B(1,1) e2(1,1) e3(1, 1) e4(1, 1) e5(1, 1) e6(1, 1);
     B(5,1) e2(5,1) e3(5, 1) e4(5, 1) e5(5, 1) e6(5, 1); 
     B(6,1) e2(6,1) e3(6, 1) e4(6, 1) e5(6, 1) e6(6, 1);]
 
+% determinant to then see conditions for determinant not equal to 0
 determinant = det(E)
